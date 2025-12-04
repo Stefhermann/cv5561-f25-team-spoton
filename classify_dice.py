bf3b814ba7d40030349e4f3be8041f4894181bfa
William Walker <24390822+superstealthysheep@users.noreply.github.com>
2025-12-04 14:33:58 -0600

    collect imgs and classes of 200 dice

--- classify_dice.py
+++ classify_dice.py
@@ -1,3 +1,4 @@
+import pathlib
 import sys
 sys.dont_write_bytecode = True
 # %load_ext autoreload
@@ -32,11 +33,31 @@ DIE = 0
 
 example_frame = "data/frames/frame_0214.jpg"
 
+def generate_dice_dataset(obb_model):
+    frames_path = pathlib.Path("data/frames")
+    dice_path = pathlib.Path("data/dice")
+
+    all_dice = []
+
+    for f_path in frames_path.glob("*.jpg"):
+        # f_path.stem
+        frame = cv2.imread(f_path)
+        yolo_res = obb_model(frame, task='obb')[0]
+        frame_dice = batch_crop_dice(frame, yolo_res)
+        all_dice.append(frame_dice)
+
+    all_dice = np.concat(all_dice, axis=0)
+    return all_dice
+
+
 def imshow(img):
     return plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 
 def infer(frame):
     ...
+
+# def make_color_patch(color, h=32,w=32)
+
 
 
 def imshow(img):
@@ -64,11 +85,29 @@ def batch_crop_dice(img, yolo_res, res_shape=(128,128)):
         cropped = crop_bb(img, bb, res_shape)
         res.append(cropped)
 
-    res = np.stack(res, axis=0)
+    if len(res) != 0:
+        res = np.stack(res, axis=0)
+    else:
+        res = np.zeros((0, res_shape[0], res_shape[1], 3))
     assert res.shape == (n_dice, res_shape[0], res_shape[1], 3) # (die, x, y, channel) -> intensity
     return res
 
-    
+def write_dice():
+    all_dice = np.load('data/dice/all_dice.npz')['arr_0']
+    out_path = pathlib.Path('data/dice/all_dice/')
+    label_path = pathlib.Path('data/dice/labeled')
+    n_dice = all_dice.shape[0]
+    np.random.seed(1337)
+    to_label = set(x.item() for x in np.random.permutation(n_dice)[:200])
+    print(to_label)
+    for die_idx in range(n_dice):
+        cv2.imwrite(out_path/f'{die_idx}.png', all_dice[die_idx,:,:,:])
+        if die_idx in to_label:
+            cv2.imwrite(label_path/f'{die_idx}.png', all_dice[die_idx,:,:,:])
+    with open(label_path/"label_template.txt", 'w') as label_file:
+        label_file.write(" \n".join(map(str, sorted(to_label))))
+
+
 
 
 if __name__ == '__main__':
