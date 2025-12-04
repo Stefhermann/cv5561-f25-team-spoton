41c576a86010703c1905b485ca5fbdd2c61908ee
William Walker <24390822+superstealthysheep@users.noreply.github.com>
2025-12-04 15:50:04 -0600

    pull more classify_dice functionality from prototype ipynb to py; add labeled dice loading function

--- classify_dice.py
+++ classify_dice.py
@@ -107,8 +107,82 @@ def write_dice():
     with open(label_path/"label_template.txt", 'w') as label_file:
         label_file.write(" \n".join(map(str, sorted(to_label))))
 
+def kmeans_color(imgs, k=16):
+    N,H,W,C = imgs.shape
+    k_means = sklearn.cluster.KMeans(n_clusters=k, random_state=1337, n_init=10)
 
+    colors_only = einops.rearrange(imgs, "N H W C -> (N H W) C")
+    k_means = k_means.fit(colors_only)
 
+    palette = []
+    for c_idx in range(k_means.cluster_centers_.shape[0]):
+        color = k_means.cluster_centers_[c_idx,:]
+        palette.append(color)
+        # plt.figure()
+        # classify_dice.imshow(color.reshape(1,1,3).astype(np.uint8))
+        # plt.show()
+    palette = np.stack(palette, axis=0).astype(np.uint8)
+    # imshow(einops.rearrange(palette, "(h w) C -> h w C", w=8))
+
+    return k_means,palette
+
+def posterize(imgs, k_means, palette):
+    N,H,W,C = imgs.shape
+    assert C == 3
+    flat_in = einops.rearrange(imgs, "N H W C -> (N H W) C")
+    flat_out = k_means.predict(flat_in)
+    flat_colors = palette[flat_out,:]
+    classes = einops.rearrange(flat_out, "(N H W) -> N H W", N=N, H=H, W=W)
+    posterized = einops.rearrange(flat_colors, "(N H W) C -> N H W C", N=N, H=H, W=W, C=C)
+
+    return classes,posterized
+
+def load_labeled_dice():
+    labeled_dice_path = pathlib.Path("data/dice/labeled")
+    labels_path = labeled_dice_path/"labels.txt"
+    labels = dict()
+    legal_player_labels = {
+        'R': 0,
+        'Y': 1,
+        'B': 2,
+    }   
+    legal_value_labels = {
+        '1': 1,
+        '2': 2,
+        '3': 3,
+        '4': 4,
+        '5': 5,
+        '6': 6,
+    }
+
+    spoiled_indices = set()
+    ids = []
+    players = []
+    values = []
+    imgs = []
+    
+    img_dict = dict()
+    for img_path in labeled_dice_path.glob("*.png"):
+        img_id = int(img_path.stem)
+        img_dict[img_id] = cv2.imread(img_path)
+
+    with open(labels_path, 'r') as label_file:
+        for line in label_file:
+            idx,player_label,value_label = line.split()
+            id = int(idx)
+            if id not in img_dict or player_label not in legal_player_labels or value_label not in legal_value_labels:
+                spoiled_indices.add(idx)
+                continue
+            ids.append(id)
+            players.append(legal_player_labels[player_label])
+            values.append(legal_value_labels[value_label])
+
+    ids = np.array(ids)
+    players = np.array(players)
+    values = np.array(values)
+    imgs = np.stack(imgs, axis=0)
+    
+    return imgs,ids,players,values
 
 if __name__ == '__main__':
     obb_model = YOLO("model/rdg_obb/weights/best.pt")
